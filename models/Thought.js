222cc2e9e2be9c97734b739d080429cbc4e50a46
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2022-01-01 20:21:27 +1100

    Added thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,35 @@
 const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.lenth;
+});
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = { Thought };
