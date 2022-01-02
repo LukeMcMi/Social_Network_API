aa2062f5270a86053a4bcbeb222f0f3098deba4d
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2022-01-02 17:57:48 +1100

    added controller to get user by id.

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -9,4 +9,21 @@ const userController = {
                 res.status(400).json(err);
             });
     },
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate('thoughts')
+        .populate('friends')
+        .select('-__v')
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No User found with this ID' });
+                return;
+            }
+            res.json(dbUserData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
 }
