02156f5daff00fad7213750c027b3f8575e844d7
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2022-01-02 18:00:30 +1100

    added create and update user to the user controller.

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -9,6 +9,7 @@ const userController = {
                 res.status(400).json(err);
             });
     },
+
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
         .populate('thoughts')
@@ -26,4 +27,24 @@ const userController = {
             res.status(400).json(err);
         });
     },
+
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
+
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this ID' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+
 }
