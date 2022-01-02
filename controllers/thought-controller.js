72fcddbe5dc1b1f674fcad8b833a6fcf64083830
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2022-01-02 16:14:28 +1100

    get all thoughts added.

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,2 +1,13 @@
 const { Thought } = require('../models/Thought');
 const User = require('../models/User');
+
+const thoughtController = {
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+}
