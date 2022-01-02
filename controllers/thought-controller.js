8bc113834af518722c628f2da68900c4363acfe4
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2022-01-02 16:38:16 +1100

    added controller to delete thought.

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -51,11 +51,33 @@ const thoughtController = {
         Thought.findOneAndUpdate({ _id: params.thoughtId }, body, { new: true, runValidators: true})
         .then(dbThoughtData => {
             if (!dbThoughtData) {
-                res.status(404).json({ message: 'No Thought found with this ID'});
+                res.status(404).json({ message: 'No Thought found with this ID' });
                 return;
             }
             res.json(dbThoughtData);
         })
         .catch(err => res.status(400).json(err));
+    },
+
+    removeThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.thoughtId })
+                   .then(deletedThought => {
+                if (!deletedThought) {
+                    return res.status(404).json({ message: 'No Thought with this ID' });
+                }
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $pull: { thoughts: params.thoughtId } },
+                    { new: true }
+                );
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this ID' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
     },
 }
