cebe3bf7b017ee9a9bb02278b5820319208e9df6
Luke McMillan <88580305+LukeMcMi@users.noreply.github.com>
2022-01-02 16:25:51 +1100

    added controller for adding thought to user."

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -25,5 +25,25 @@ const thoughtController = {
             console.log(err);
             res.status(400).json(err);
         });
-    }
+    },
+    
+    addThought({ params, body }, res) {
+        console.log(body);
+        Thought.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { _id: params.userId },
+                { $push: { thoughts: _id } },
+                { new: true }
+            );
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this ID'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
 }
